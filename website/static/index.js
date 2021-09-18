9dbfbc3b05e1d771b140a3b2c95782c5626a4ca6
programmingwithcybineer <programmingwithcybineer@gmail.com>
2021-09-17 20:02:51 -0400

    initial commit

---
+++ website/static/index.js
@@ -0,0 +1,13 @@
+function delete_note(noteId){
+    fetch("/delete-note",{
+        method: "POST",
+        body: JSON.stringify({ noteId: noteId })
+    }).then((_res) => {
+        window.location.href = "/";
+    });
+}
+
+
+
+
+
